8ad63630ad5881170f07a969f8cd1b102744a520
ileuskov <ileuskov@gmail.com>
2020-04-04 19:50:31 +0200

    removed some comments

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,30 +1,3 @@
-// var bodyParser = require("body-parser");
-
-// var data = [{item: "learn Front end"}, {item: "learn Back end"}, {item: "become a full stack developer"}];
-// var urlencodedParser = bodyParser.urlencoded({extended: false});
-
-// module.exports = function (app) {
-
-//     // handling the get requests
-//     app.get("/todo", function (req, res) {
-//         res.render("todo", {todos: data});
-//     });
-
-//     // handling the post requests
-//     app.post("/todo", urlencodedParser, function (req, res) {
-//         data.push(req.body);
-//         res.json(data);
-//     });
-
-//     // handling the post requests
-//     app.delete("/todo", function (req, res) {
-//         data = data.filter(function(todo){
-//             return todo.item.replace(/ /g, "-") !== req.params.item;
-//         });
-//         res.json({todos: data});
-//     });
-// }
-
 // In this controller we'd be setting up routing and some other basic functionalities of our app.
 
 // Importing body-parser
@@ -49,7 +22,7 @@ module.exports = function(app){
         // Adding the items to the top of the list instead :P
         data.unshift(req.body);
         res.json(data);
-        // res.render('todo', {todos: data});
+
     });
 
     // For when a user sends a delete request
